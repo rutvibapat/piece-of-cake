5d57c99ab9a0f383ce655c71468aa8f66c7f14bd
rutvi bapat <rutvi30703@gmail.com>
2022-08-30 12:41:06 +0400

    pieceofcake

---
+++ Sketch.js
@@ -0,0 +1,15 @@
+function packcake(){
+    console.log("ok")
+   document.getElementById("wholecake").innerHTML = `<img src="packcake.gif" width="900" height="600" style="margin-left:375px">`
+   }
+
+    function bite(){
+        document.getElementById("wholecake").innerHTML = `<img src="pieceofcake.gif" width="900" height="600" style="margin-left:375px">`
+    }
+function deliver(){
+        
+       document.getElementById("wholecake").innerHTML = `<img src="delivery.gif" width="900" height="600" style="margin-left:375px">`
+       }
+
+
+  
